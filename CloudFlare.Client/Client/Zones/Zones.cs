a8da17d1c352af8e89322bc7e3388b3209b56d84
captainrhubarb <65362626+captainrhubarb@users.noreply.github.com>
2024-11-22 12:31:11 -0800

    Cache purge by url (#1)
    * Add Cache Purge By Url
    
    * Added null safety
    
    * Add overload
    
    * Use IEnumerable
    
    ---------
    
    Co-authored-by: Mark Lagendijk <mark@lagendijk.nl>

--- CloudFlare.Client/Client/Zones/Zones.cs
+++ CloudFlare.Client/Client/Zones/Zones.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CloudFlare.Client.Api.Display;
@@ -104,6 +105,41 @@ namespace CloudFlare.Client.Client.Zones
 
             var requestUri = $"{ZoneEndpoints.Base}/{zoneId}/{ZoneEndpoints.PurgeCache}";
             return await Connection.PostAsync<Zone, Dictionary<string, bool>>(requestUri, content, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public Task<CloudFlareResult<Zone>> PurgeFilesAsync(string zoneId, IEnumerable<string> files, CancellationToken cancellationToken = default)
+        {
+            return PurgeFilesAsync(
+                zoneId,
+                files
+                    .Select(url => new CachePurgeFile { Url = url, Headers = new Dictionary<string, string>() })
+                    .ToList(),
+                cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public async Task<CloudFlareResult<Zone>> PurgeFilesAsync(string zoneId, IEnumerable<CachePurgeFile> files, CancellationToken cancellationToken = default)
+        {
+            var content = new Dictionary<string, IReadOnlyList<CachePurgeFile>>
+            {
+                {
+                    Outgoing.Files,
+                    files
+                        .Select(file => new CachePurgeFile
+                        {
+                            Url = file.Url,
+                            Headers = file.Headers == null ?
+                                new Dictionary<string, string>() :
+                                file.Headers
+                                    .Where(header => header.Value != null)
+                                    .ToDictionary(header => header.Key, header => header.Value)
+                        })
+                        .ToList()
+                }
+            };
+            var requestUri = $"{ZoneEndpoints.Base}/{zoneId}/{ZoneEndpoints.PurgeCache}";
+            return await Connection.PostAsync<Zone, Dictionary<string, IReadOnlyList<CachePurgeFile>>>(requestUri, content, cancellationToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
